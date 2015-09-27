34a39c14c33ae4b8b4d7544014af22e35c6f363f
davidjanke <davidjanke@gmail.com>
2015-09-27 17:28:34 +0200

    Ready, except maybe some errors

--- SuperTable.java
+++ SuperTable.java
@@ -8,55 +8,120 @@ import org.apache.hadoop.hbase.HTableDescriptor;
 
 import org.apache.hadoop.hbase.TableName;
 
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
-
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class SuperTable{
 
-   public static void main(String[] args) throws IOException {
+    public static final String POWERSTABLE_NAME = "powers";
+    public static final String COLUMNFAMILIY_PERSONAL = "personal";
+    public static final String COLUMNFAMILIY_PROFESSIONAL = "professional";
 
-      // Instantiate Configuration class
+    private static Configuration mp4Config = HBaseConfiguration.create();
 
-      // Instaniate HBaseAdmin class
-      
-      // Instantiate table descriptor class
+    private static HBaseAdmin hbaseAdmin;
 
-      // Add column families to table descriptor
+    private static void init()  {
+        // Instaniate HBaseAdmin class
+        try {
+            hbaseAdmin =  new HBaseAdmin(mp4Config);
+        } catch (IOException e) {
+            handleCriticalError("Could not create HBase admin", e);
+        }
 
-      // Execute the table through admin
+        // Execute the table through admin
+        instantiateTableDescriptor();
 
-      // Instantiating HTable class
-     
-      // Repeat these steps as many times as necessary
+    }
 
-	      // Instantiating Put class
-              // Hint: Accepts a row name
 
-      	      // Add values using add() method
-              // Hints: Accepts column family name, qualifier/row name ,value
+    public static void main(String[] args) throws IOException {
 
-      // Save the table
-	
-      // Close table
+        init();
 
-      // Instantiate the Scan class
-     
-      // Scan the required columns
+        // Instantiating HTable class
+        HTable table = getOrCreateTable(POWERSTABLE_NAME);
 
-      // Get the scan result
+        // Repeat these steps as many times as necessary
+        table.put(rowOf("row1", "superman", "strength", "clark", "100"));
+        table.put(rowOf("row2", "batman", "money", "bruce", "50"));
+        table.put(rowOf("row3", "wolverine", "healing", "logan", "75"));
 
-      // Read values from scan result
-      // Print scan result
- 
-      // Close the scanner
-   
-      // Htable closer
-   }
+        // Close table
+        table.close();
+
+        // Instantiate the Scan class
+        Scan scanner = new Scan();
+
+        // Scan the required columns
+        scanner.addColumn(Bytes.toBytes(COLUMNFAMILIY_PERSONAL), Bytes.toBytes("hero"));
+
+        // Get the scan result
+        ResultScanner results = table.getScanner(scanner);
+
+        // Read values from scan result
+        for(Result row = results.next(); null != row; results.next()) {
+            // Print scan result
+            printRow(row);
+        }
+
+        // Close the scanner
+        results.close();
+
+        // Htable closer
+
+    }
+
+    private static void printRow(Result row) {
+        System.out.println(row.toString());
+    }
+
+    public static Put rowOf(String rowId, String hero, String power, String name, String xp) {
+        Put put = new Put(Bytes.toBytes(rowId));
+        put.add(Bytes.toBytes(COLUMNFAMILIY_PERSONAL), Bytes.toBytes(hero), Bytes.toBytes(power));
+        put.add(Bytes.toBytes(COLUMNFAMILIY_PROFESSIONAL), Bytes.toBytes(name), Bytes.toBytes(xp));
+    }
+
+    private static void instantiateTableDescriptor() {
+        if(checkIfTableDoesNotExist(POWERSTABLE_NAME)) {
+            try {
+                hbaseAdmin.createTable(createTableDescriptor());
+            } catch (IOException e) {
+                handleCriticalError("Could not create table descriptor", e);
+            }
+        }
+    }
+
+    private static boolean checkIfTableDoesNotExist(String tableName) {
+        try {
+            return !hbaseAdmin.tableExists(tableName);
+        } catch (IOException e) {
+            handleCriticalError("Error when checking for table", e);
+        }
+    }
+
+    private static HTableDescriptor createTableDescriptor() {
+        // Instantiate table descriptor class
+        HTableDescriptor mp4TableDescriptor = new HTableDescriptor(TableName.valueOf(POWERSTABLE_NAME));
+
+        // Add column families to table descriptor
+        mp4TableDescriptor.addFamily(new HColumnDescriptor(COLUMNFAMILIY_PERSONAL));
+        mp4TableDescriptor.addFamily(new HColumnDescriptor(COLUMNFAMILIY_PROFESSIONAL));
+
+        return mp4TableDescriptor;
+
+    }
+
+    private static HTable getOrCreateTable(String tableName) {
+        try {
+            return new HTable(mp4Config, tableName);
+        } catch (IOException e) {
+            handleCriticalError("Could not access or create table " + tableName, e);
+        }
+    }
+
+    private static void handleCriticalError(String message, IOException e) {
+        throw new RuntimeException(message, e);
+    }
 }
 
